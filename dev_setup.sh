8d001b52b4c2f1ce06237075cde67e29523d5f82
katruya <kybryce4@gmail.com>
2021-06-11 14:48:25 -0500

    finished dev_setup v1.0

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
+# Script to be run on a fresh install of Mint/Ubuntu/Debian distros
 
-# Script to be run on a fresh install
+# First, update/upgrade
+sudo apt update && sudo apt -y upgrade
+
+# Applications
+sudo apt install -y google-chrome-stable
 
 # Development Essentials
-sudo apt update && sudo apt upgrade
-sudo apt install code
-sudo apt install g++
-sudo apt install python3
-sudo apt install openjdk8
+sudo apt install -y code
+sudo apt install -y vim
+sudo apt install -y git
+sudo apt install -y g++
+sudo apt install -y python3
 sudo apt install -y nodejs
-sudo apt install pip
-sudo apt install mysql-shell
-sudo apt update && sudo apt upgrade
+sudo apt install -y npm
+sudo apt install -y pip
+sudo apt install -y mysql-server
+
+# Make sure everything is fully up to date
+sudo apt update && sudo apt -y upgrade
+
+# Edit your .bashrc (add aliases, functions, etc)
+read -p "Do you want to edit your .bashrc? (y/n)" input
+
+if [ "$input" == 'y' ]
+then
+    vim ~/.bashrc
+    source ~/.bashrc 
+fi
+
+
+echo "Completed First Time Setup."
+# end
